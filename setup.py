a1ec5c9d8e12268f2848d37fab585c7c7664b319
Alexander Artemenko <svetlyak.40wt@gmail.com>
2009-05-29 16:08:26 +0400

    Code was moved to 'src'. Added setup.py and MANIFEST.in.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'django-fields',
+    version = '0.1.0',
+    description = 'Django-fields is an application which includes different kinds of models fields.',
+    keywords = 'django apps tools collection',
+    license = 'New BSD License',
+    author = 'Alexander Artemenko',
+    author_email = 'svetlyak.40wt@gmail.com',
+    url = 'http://github.com/svetlyak40wt/django-fields/',
+    install_requires = ['pycrypto', ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Plugins',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    package_dir = {'': 'src'},
+    packages = ['django_fields'],
+    include_package_data = True,
+)
+
+
