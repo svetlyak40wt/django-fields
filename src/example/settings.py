ac10b3fe6c62d649ac9dcf75a496527686fede30
Alexander Artemenko <svetlyak.40wt@gmail.com>
2011-01-21 22:31:26 +0300

    Fix deprecation warnings related to the settings in example project.

--- src/example/settings.py
+++ src/example/settings.py
@@ -9,12 +9,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'test.sqlite'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = dict(
+    default = dict(
+        ENGINE = 'django.db.backends.sqlite3',
+        NAME = 'test.sqlite',
+    )
+)
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
