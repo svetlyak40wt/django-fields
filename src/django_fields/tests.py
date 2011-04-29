d23e69f0114326385515b1c57b30f95827930dd8
Joe Jasinski <joe.jasinski@gmail.com>
2011-04-29 01:08:30 -0500

    adding tests... not working yet

--- src/django_fields/tests.py
+++ src/django_fields/tests.py
@@ -7,8 +7,10 @@ import unittest
 from django.db import connection
 from django.db import models
 
-from fields import EncryptedCharField, EncryptedDateField, EncryptedDateTimeField, EncryptedIntField, EncryptedLongField, EncryptedFloatField, PickleField
-
+from fields import (EncryptedCharField, EncryptedDateField, 
+                    EncryptedDateTimeField, EncryptedIntField, 
+                    EncryptedLongField, EncryptedFloatField, PickleField,
+                    EncryptedUSPhoneNumberField, EncryptedEmailField)
 
 class EncObject(models.Model):
     max_password = 20
@@ -39,6 +41,15 @@ class EncFloat(models.Model):
 class PickleObject(models.Model):
     name = models.CharField(max_length=16)
     data = PickleField()
+
+
+class EmailObject(models.Model): 
+    max_email = 255
+    email = EncryptedEmailField()
+
+
+class USPhoneNumberField(models.Model):
+    phone = EncryptedUSPhoneNumberField()
 
 
 class EncryptTests(unittest.TestCase):
@@ -248,3 +259,89 @@ class TestPickleField(unittest.TestCase):
         for name, data in DATA:
             obj = PickleObject.objects.get(name=name)
             self.assertEqual(obj.data, data)
+
+
+
+
+class EncryptEmailTests(unittest.TestCase):
+
+    def setUp(self):
+        EmailObject.objects.all().delete()
+
+    def test_encryption(self):
+        """
+        Test that the database values are actually encrypted.
+        """
+        email = 'test@example.com'  # 16 chars
+        obj = EmailObject(email = email)
+        obj.save()
+        # The value from the retrieved object should be the same...
+        obj = EmailObject.objects.get(id=obj.id)
+        self.assertEqual(email, obj.email)
+        # ...but the value in the database should not
+        encrypted_email = self._get_encrypted_email(obj.id)
+        self.assertNotEqual(encrypted_email, email)
+        self.assertTrue(encrypted_email.startswith('$AES$'))
+
+    def test_max_field_length(self):
+        email = 'a' * EmailObject.max_email
+        obj = EmailObject(email = email)
+        obj.save()
+        obj = EmailObject.objects.get(id=obj.id)
+        self.assertEqual(email, obj.email)
+
+    def test_field_too_long(self):
+        email = 'a' * (EmailObject.max_email + 1)
+        obj = EmailObject(email = email)
+        self.assertRaises(Exception, obj.save)
+
+    def test_UTF8(self):
+        email = u'совершенно@секретно.com'
+        obj = EmailObject(email = email)
+        obj.save()
+        obj = EmailObject.objects.get(id=obj.id)
+        self.assertEqual(email, obj.email)
+    
+    def test_consistent_encryption(self):
+        """
+        The same password should not encrypt the same way twice.
+        Check different lengths.
+        """
+        # NOTE:  This may fail occasionally because the randomly-generated padding could be the same for both values.
+        # A 14-char string will only have 1 char of padding.  There's a 1/len(string.printable) chance of getting the
+        # same value twice.
+        for email_length in range(1,21):  # 1-20 inclusive
+            enc_email_1, enc_email_2 = self._get_two_emails(email_length)
+            self.assertNotEqual(enc_email_1, enc_email_2)
+    
+    def test_minimum_padding(self):
+        """
+        There should always be at least two chars of padding.
+        """
+        enc_field = EncryptedCharField()
+        for pwd_length in range(1,21):  # 1-20 inclusive
+            email = 'a' * pwd_length  # 'a', 'aa', ...
+            self.assertTrue(enc_field._get_padding(email) >= 2)
+
+    ### Utility methods for tests ###
+
+    def _get_encrypted_email(self, id):
+        cursor = connection.cursor()
+        cursor.execute("select email from django_fields_encobject where id = %s", [id,])
+        emails = map(lambda x: x[0], cursor.fetchall())
+        self.assertEqual(len(emails), 1)  # only one
+        return emails[0]
+
+    def _get_two_emails(self, email_length):
+        email = 'a' * email_length  # 'a', 'aa', ...
+        obj_1 = EmailObject(email = email)
+        obj_1.save()
+        obj_2 = EmailObject(email = email)
+        obj_2.save()
+        # The encrypted values in the database should be different.
+        # There's a chance they'll be the same, but it's small.
+        enc_email_1 = self._get_encrypted_email(obj_1.id)
+        enc_email_2 = self._get_encrypted_email(obj_2.id)
+        return enc_email_1, enc_email_2
+
+
