3d9e1e4b5c950b27117373de92e5dea5e8ac78b9
Bryan <bryan@bryanhelmig.com>
2011-12-06 15:22:51 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
+    name = 'django-fields',
+    version = '0.1.4',
+    description = 'Django-fields is an application which includes different kinds of models fields.',
+    keywords = 'django apps tools collection',
+    license = 'New BSD License',
+    author = 'Alexander Artemenko',
+    author_email = 'svetlyak.40wt@gmail.com',
     url = 'http://github.com/bryanhelmig/django-fields/',
-    download_url='https://github.com/bryanhelmig/django-fields/',
     install_requires = ['pycrypto', ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Plugins',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
     package_dir = {'': 'src'},
     packages = ['django_fields'],
     #include_package_data = True,
 )
-
-
