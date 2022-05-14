1fecf5b58a15abcaf27d97435ace86c0ec7bd229
Serg Tereshchenko <serg.partizan@gmail.com>
2022-05-14 14:23:19 +0300

    chore: Fix deprecation

--- src/django_fields/fields.py
+++ src/django_fields/fields.py
@@ -9,7 +9,7 @@ from django import forms
 from django.forms import fields
 from django.db import models
 from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from Cryptodome import Random
 from Cryptodome.Random import random
 
